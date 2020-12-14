25d910853d805f0a12b883da0ee460caa11391dc
Jotape <jpmgomes95@gmail.com>
2020-12-14 16:08:46 -0300

    I just changed the language of the comments to english and improved the "while" loop

--- adivinhacao.py
+++ adivinhacao.py
@@ -1,24 +1,21 @@
 print("*****************************************")
-print("Bem vindo ao jogo de adivinhação") #teste inicial para verificar o python em funcionamento
+print("Welcome to the guessing game") #initial test of python and pycharm
 print("*****************************************")
 
 numero_secreto = 42
 
-chute_str = input("Digite o seu número: ") #o input retorna strings, portanto devemos modificar essa entrada
-#com uma função, no caso a função abaixo.
+chute_str = input("Digite o seu número: ") #As the input returns only strings you have to use a function to change its type
 
-chute =int(chute_str)
-
-print ("Você digitou : ", chute, "?")
+print ("Você digitou : ", chute_str, "?")
 
 resposta = input("Se sim digite 'sim' : ")
 
 while(resposta != "sim"):
     chute_str = input("Digite o seu número: ")
-    print("Você digitou : ", chute, "?")
+    print("Você digitou : ", chute_str, "?")
     resposta = input("Se sim digite 'sim' : ")
 
-chute = int(chute_str)
+chute = int(chute_str) #Right here i used the function that changes the input to an int type
 
 if(numero_secreto == chute ):
     print("Você acertou o número secreto !")
