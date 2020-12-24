9cb25d13c9aa472ac8dd17155dfdae3b02f8eb89
Jotape <jpmgomes95@gmail.com>
2020-12-24 04:23:23 -0300

    Here i start my study with type str and its methods

--- forca.py
+++ forca.py
@@ -3,6 +3,26 @@ def  play():
     print("Welcome to the Hangman") #initial test of python and pycharm
     print("*****************************************")
 
+    secret_word = "banana"
+    hanged = False
+    miss = False
+
+    secret_word = secret_word.strip()
+
+    while( not hanged and not miss ):
+
+        guess =  input("Say a letter: ")
+        guess = guess.strip()
+
+        index = 0
+
+        for letter in secret_word :
+            if (guess.lower() == letter.lower()):
+                print("Congratulations you hit the letter {} in position {} ".format(guess, index))
+            index = index + 1
+
+
+
     print("End game")
 
 if(__name__== "__main__"):
