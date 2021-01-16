64e67594307c5e57502a866bb2458185ae119f9b
Jotape <jpmgomes95@gmail.com>
2021-01-16 12:22:31 -0300

    added few more functions, so the code can be easly readen

--- forca.py
+++ forca.py
@@ -1,5 +1,37 @@
 import  random
 
+def  play():
+
+    opening_message()
+    secret_word = load_secret_word()
+    correct_letters = load_corretct_letters(secret_word)
+
+
+    hanged = False
+    right = False
+    mistakes = 0
+
+    secret_word = secret_word.strip()
+
+
+    while( not hanged and not right ):
+        guess = ask_the_gues()
+
+        if (guess in secret_word):
+            score_right_guess(guess,correct_letters,secret_word)
+        else:
+            print("You have mistaken, there is'nt  {}, in this secret letter".format(guess))
+            mistakes +=1
+
+        hanged = mistakes == len(secret_word)
+        right = "_" not in correct_letters
+        print(correct_letters)
+
+    if(right):
+        print("Congratulations you finished the game!")
+    else:
+        print("You lost the game")
+
 def opening_message():
     print("*****************************************")
     print("Welcome to the Hangman")  # initial test of python and pycharm
@@ -25,44 +57,20 @@ def load_secret_word():
 def load_corretct_letters(word):
     return ["_" for letter in word]
 
-def  play():
 
-    opening_message()
-    secret_word = load_secret_word()
-    correct_letters = load_corretct_letters(secret_word)
+def ask_the_gues():
+    guess = input("Say a letter: ")
+    guess = guess.strip().upper()
+    return guess
 
+def score_right_guess(guess,correct_letters,secret_word):
+    index = 0
+    for letter in secret_word:
+        if (guess == letter):
+            print("Congratulations you hit the letter {} in position {} ".format(guess, index))
+            correct_letters[index] = guess
+        index += 1
 
-    hanged = False
-    right = False
-    mistakes = 0
-
-    secret_word = secret_word.strip()
-
-
-    while( not hanged and not right ):
-        guess =  input("Say a letter: ")
-        guess = guess.strip().upper()
-
-        if (guess in secret_word):
-            index = 0
-            for letter in secret_word :
-                if (guess == letter):
-                    print("Congratulations you hit the letter {} in position {} ".format(guess, index))
-                    correct_letters[index] = guess
-
-                index += 1
-        else:
-            print("You have mistaken, there is'nt  {}, in this secret letter".format(guess))
-            mistakes +=1
-
-        hanged = mistakes == len(secret_word)
-        right = "_" not in correct_letters
-        print(correct_letters)
-
-    if(right):
-        print("Congratulations you finished the game!")
-    else:
-        print("You lost the game")
 
 
 if(__name__== "__main__"):
